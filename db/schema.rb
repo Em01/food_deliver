f125f6ee0559b3783fe320a14fc343b2b56c9b06
Em01 <emma_williams1@hotmail.co.uk>
2014-10-31 15:25:23 +0000

    set up for carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031151334) do
+ActiveRecord::Schema.define(version: 20141031152303) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141031151334) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
